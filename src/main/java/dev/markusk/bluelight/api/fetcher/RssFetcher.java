0601ca279fb091db21683a455759d98f351fdae9
Markus <m.kording@hotmail.de>
2020-03-13 22:21:57 +0100

    Add basic extractDate method

--- src/main/java/dev/markusk/bluelight/api/fetcher/RssFetcher.java
+++ src/main/java/dev/markusk/bluelight/api/fetcher/RssFetcher.java
@@ -5,29 +5,37 @@ import com.apptastic.rssreader.RssReader;
 import dev.markusk.bluelight.api.AbstractInfoFetcher;
 import dev.markusk.bluelight.api.builder.BaseFetchInfoBuilder;
 import dev.markusk.bluelight.api.objects.BaseFetchInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.stream.Stream;
 
-public abstract class RssFetcher implements AbstractInfoFetcher {
+public class RssFetcher implements AbstractInfoFetcher {
+
+  private static final Logger LOGGER = LogManager.getLogger();
 
   private RssReader rssReader;
 
   private String targetUid;
   private String url;
+  private String simpleDatePattern = "E',' dd MMM yyyy HH:mm:ss Z";
+
+  private SimpleDateFormat simpleDateFormat;
 
   @Override
   public void initialize(final String targetUid, final String fetchUrl) {
     this.rssReader = new RssReader();
     this.targetUid = targetUid;
     this.url = fetchUrl;
+    this.simpleDateFormat = new SimpleDateFormat(this.simpleDatePattern, Locale.US);
+    this.simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
   }
 
   @Override
-  public List<BaseFetchInfo> getFetchInfos() throws IOException {
+  public List<BaseFetchInfo> getFetchInfos() throws Exception {
     final Stream<Item> read = this.rssReader.read(this.getFetchUrl());
     final List<BaseFetchInfo> fetchInfos = new ArrayList<>();
     read.forEach(item -> {
@@ -52,6 +60,14 @@ public abstract class RssFetcher implements AbstractInfoFetcher {
     return this.targetUid;
   }
 
+  public String getSimpleDatePattern() {
+    return simpleDatePattern;
+  }
+
+  public void setSimpleDatePattern(final String simpleDatePattern) {
+    this.simpleDatePattern = simpleDatePattern;
+  }
+
   /**
    * @param inputDate is a formatted date string
    *                  This string can be null. This means that the pubDate could not be extracted from the RSS feed.
@@ -59,6 +75,15 @@ public abstract class RssFetcher implements AbstractInfoFetcher {
    *                  Example: Fri, 13 Mar 2020 19:40:45 +0100
    * @return a date object in utc time zone!
    */
-  public abstract Date extractDate(String inputDate);
+  public Date extractDate(String inputDate) {
+    final Date parse;
+    try {
+      parse = this.simpleDateFormat.parse(inputDate);
+    } catch (ParseException e) {
+      LOGGER.error("Error while parsing date", e);
+      return null;
+    }
+    return parse;
+  }
 
 }
